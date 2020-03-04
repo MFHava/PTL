29a66c6bcbdb7d96bad936baf99874afc06a93e0
MFHava <mfh@live.at>
2020-03-04 14:05:52 +0100

    fixed constexpr-capturing issue

--- inc/ptl/internal/fnv.hpp
+++ inc/ptl/internal/fnv.hpp
@@ -13,15 +13,18 @@ namespace ptl::internal {
 	template<std::size_t Size>
 	constexpr
 	auto fnv1a(const std::uint8_t (& values)[Size]) noexcept -> std::size_t {
+		static_assert(CHAR_BIT == 8);
 		constexpr auto bitness{8 * sizeof(std::size_t)};
 		static_assert(bitness == 32 || bitness == 64);
 
 		constexpr auto offset_basis{[&] {
+			constexpr auto bitness{8 * sizeof(std::size_t)};
 			if constexpr(bitness == 32) return std::uint32_t{2166136261U};
 			else return std::uint64_t{14695981039346656037ULL};
 		}()};
 
 		constexpr auto prime{[&] {
+			constexpr auto bitness{8 * sizeof(std::size_t)};
 			if constexpr(bitness == 32) return std::uint32_t{16777619U};
 			else return std::uint64_t{1099511628211ULL};
 		}()};

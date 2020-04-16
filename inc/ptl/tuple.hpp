1d66e358a128c7a130be366948159f756c94f65e
MFHava <mfh@live.at>
2020-04-16 02:10:00 +0200

    refactored tuple  * storage_type is determined internally

--- inc/ptl/tuple.hpp
+++ inc/ptl/tuple.hpp
@@ -5,7 +5,10 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 #pragma once
-#include "internal/tuple.hpp"
+#include "type_list.hpp"
+#include "internal/type_checks.hpp"
+#include "internal/cpp20_emulation.hpp"
+#include "internal/compiler_detection.hpp"
 
 namespace ptl {
 	PTL_PACK_BEGIN
@@ -13,7 +16,68 @@ namespace ptl {
 	//! @tparam Types types to store inside the tuple
 	template<typename... Types>
 	class tuple final {
-		internal::tuple_storage<Types...> storage;
+		static_assert((internal::is_abi_compatible_v<Types> && ...));
+
+		struct empty_base {
+			static
+			constexpr
+			void swap(empty_base &) noexcept {}
+
+			static
+			constexpr
+			auto equal(const empty_base &) noexcept { return true; }
+			static
+			constexpr
+			auto less(const empty_base &) noexcept { return false; }
+		};
+
+		template<typename Type, typename Base>
+		struct base : Base {
+			template<typename Arg, typename... Args>
+			base(Arg && arg, Args &&... args) : Base{std::forward<Args>(args)...}, value{std::forward<Arg>(arg)} {}
+
+			template<std::size_t Index>
+			constexpr
+			auto get() const noexcept -> decltype(auto) {
+				if constexpr(Index == 0) return (value);
+				else return Base::template get<Index - 1>();
+			}
+			template<std::size_t Index>
+			constexpr
+			auto get()       noexcept -> decltype(auto) {
+				if constexpr(Index == 0) return (value);
+				else return Base::template get<Index - 1>();
+			}
+
+			constexpr
+			void swap(base & other) noexcept {
+				using std::swap;
+				swap(value, other.value);
+				Base::swap(other);
+			}
+
+			constexpr
+			auto equal(const base & other) const noexcept { return (value == other.value) ? Base::equal(other) : false; }
+			constexpr
+			auto less(const base & other) const noexcept { return (value < other.value) || (!(other.value < value) && Base::less(other)); }
+		private:
+			Type value;
+		};
+
+		template<typename TL, typename Base = empty_base>
+		static
+		constexpr //TODO(C++20): consteval
+		auto determine_storage() noexcept {
+			if constexpr(TL::empty) return internal::identity_type<Base>{};
+			else {
+				using Head = typename TL::template at<0>;
+				using Tail = typename TL::template erase_at<0>;
+				constexpr auto tmp{determine_storage<Tail, Base>()};
+				return internal::identity_type<base<Head, typename decltype(tmp)::type>>{};
+			}
+		}
+
+		typename decltype(determine_storage<type_list<Types...>>())::type storage;
 
 		template<std::size_t Index, typename... Ts>
 		friend
@@ -30,18 +94,6 @@ namespace ptl {
 		tuple(Args &&... args) : storage{std::forward<Args>(args)...} {}
 
 		constexpr
-		tuple(const tuple &) =default;
-		constexpr
-		tuple(tuple &&) noexcept =default;
-
-		constexpr
-		auto operator=(const tuple &) -> tuple & =default;
-		constexpr
-		auto operator=(tuple &&) noexcept -> tuple & =default;
-
-		~tuple() noexcept =default;
-
-		constexpr
 		void swap(tuple & other) noexcept { storage.swap(other.storage); }
 		friend
 		constexpr
@@ -49,13 +101,13 @@ namespace ptl {
 
 		friend
 		constexpr
-		auto operator==(const tuple & lhs, const tuple & rhs) noexcept { return lhs.storage == rhs.storage; }
+		auto operator==(const tuple & lhs, const tuple & rhs) noexcept { return lhs.storage.equal(rhs.storage); }
 		friend
 		constexpr
 		auto operator!=(const tuple & lhs, const tuple & rhs) noexcept { return !(lhs == rhs); }
 		friend
 		constexpr
-		auto operator< (const tuple & lhs, const tuple & rhs) noexcept { return lhs.storage <  rhs.storage; }
+		auto operator< (const tuple & lhs, const tuple & rhs) noexcept { return lhs.storage.less(rhs.storage); }
 		friend
 		constexpr
 		auto operator> (const tuple & lhs, const tuple & rhs) noexcept { return rhs < lhs; }

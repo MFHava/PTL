7e282a7b67fc9fc9ebe42974e2a8dac4b876a613
MFHava <mfh@live.at>
2020-11-24 21:38:54 +0100

    refactored function_ref  * dropped usage of packing-macros (should not be necessary)  * dropped internal helper types  * new internal shared base class   - unifies logic for throwing/non-throwing functions   - exists only as noexceptness not being deducable  * function_ref is now standalone  * added several TODOs for future development

--- inc/ptl/function_ref.hpp
+++ inc/ptl/function_ref.hpp
@@ -5,54 +5,94 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 #pragma once
-#include "internal/function_ref.hpp"
-#include "internal/compiler_detection.hpp"
+#include <utility>
+#include <type_traits>
 
 namespace ptl {
-	static_assert(sizeof(void *) == sizeof(void(*)()), "implementation assumes that function pointers have the same size as object pointers");
+	static_assert(sizeof(void *) == sizeof(void(*)()));
+	static_assert(sizeof(void *) == sizeof(void(*)() noexcept));
 
-	PTL_PACK_BEGIN
 	//! @brief non-owning reference to a functor (either a plain function or a lambda)
 	//! @tparam Signature function signature of the referenced functor (including potential noexcept-qualifier)
 	template<typename Signature>
-	class function_ref final {
-		using traits = internal::function_ref_traits<Signature>;
+	class function_ref;
 
-		void * functor;
-		typename traits::dispatch_type * dispatch;
-	public:
-		template<typename Functor, typename = std::enable_if_t<!std::is_same_v<internal::remove_cvref_t<Functor>, function_ref> && traits::template compatible<Functor>>>
-		constexpr
-		function_ref(Functor && func) noexcept {
-			if constexpr(std::is_pointer_v<Functor>) {
-				//pre-condition: func
-				functor = reinterpret_cast<void *>(func);
-				dispatch = traits::template dispatcher<std::remove_pointer_t<Functor>>;
-			} else {
-				functor = reinterpret_cast<void *>(std::addressof(func));
-				dispatch = traits::template dispatcher<Functor>;
+	namespace internal { //TODO: [C++??] if noexcept-ness were deducible, this extra class would not be necessary and the code overall would be simpler
+		template<bool Noexcept, typename Result, typename... Args>
+		class function_ref {
+			void * functor;
+			Result (*dispatch)(void *, Args...) noexcept(Noexcept);
+		public:
+			template<typename Functor>
+			constexpr
+			function_ref(Functor && func) noexcept {
+				if constexpr(std::is_pointer_v<Functor>) {
+					//TODO: [C++??] precondition(func);
+					functor = reinterpret_cast<void *>(func);
+				} else functor = reinterpret_cast<void *>(std::addressof(func));
+				dispatch = [](void * ctx, Args... args) noexcept(Noexcept) { return (*reinterpret_cast<std::conditional_t<std::is_pointer_v<Functor>, Functor, std::remove_reference_t<Functor> *>>(ctx))(std::forward<Args>(args)...); };
 			}
-		}
 
-		constexpr
-		void swap(function_ref & other) noexcept {
-			std::swap(functor,  other.functor);
-			std::swap(dispatch, other.dispatch);
-		}
-		friend
-		constexpr
-		void swap(function_ref & lhs, function_ref & rhs) noexcept { lhs.swap(rhs); }
+			constexpr
+			void swap(function_ref & other) noexcept {
+				std::swap(functor,  other.functor);
+				std::swap(dispatch, other.dispatch);
+			}
+			friend
+			constexpr
+			void swap(function_ref & lhs, function_ref & rhs) noexcept { lhs.swap(rhs); }
 
-		template<typename... Args, typename = std::enable_if_t<std::is_invocable_v<Signature, Args...>>>
-		auto operator()(Args &&... args) const noexcept(traits::noexcept_) -> typename traits::result_type { return dispatch(functor, std::forward<Args>(args)...); }
+			auto operator()(Args... args) const noexcept(Noexcept) -> Result { return dispatch(functor, std::forward<Args>(args)...); }
+		};
+	}
+
+	template<typename Result, typename... Args>
+	struct function_ref<Result(Args...)> final : internal::function_ref<false, Result, Args...> { //TODO: static_assert(sizeof(function_ref<T>) == 2 * sizeof(void *));
+		template<typename Functor, typename = std::enable_if_t<std::is_invocable_r_v<Result, Functor &, Args...>>>
+		constexpr
+		function_ref(Functor && func) noexcept : internal::function_ref<false, Result, Args...>{std::forward<Functor>(func)} {}
 	};
-	PTL_PACK_END
+
+	template<typename Result, typename... Args>
+	struct function_ref<Result(Args...) noexcept> final : internal::function_ref<true, Result, Args...> { //TODO: static_assert(sizeof(function_ref<T>) == 2 * sizeof(void *));
+		template<typename Functor, typename = std::enable_if_t<std::is_nothrow_invocable_r_v<Result, Functor &, Args...>>>
+		constexpr
+		function_ref(Functor && func) noexcept : internal::function_ref<true, Result, Args...>{std::forward<Functor>(func)} {}
+	};
 
 	template<typename Result, typename... Args>
 	function_ref(Result(*)(Args...)) -> function_ref<Result(Args...)>;
 
 	template<typename Result, typename... Args>
 	function_ref(Result(*)(Args...) noexcept) -> function_ref<Result(Args...) noexcept>;
+
+	namespace internal {
+		template<typename Func>
+		struct deduce_lambda;
+
+		template<typename Func>
+		using deduce_lambda_t = typename deduce_lambda<Func>::type;
+
+		template<typename Result, typename Class, typename... Args>
+		struct deduce_lambda<Result(Class::*)(Args...)> final {
+			using type = Result(Args...);
+		};
+
+		template<typename Result, typename Class, typename... Args>
+		struct deduce_lambda<Result(Class::*)(Args...) const> final {
+			using type = Result(Args...);
+		};
+
+		template<typename Result, typename Class, typename... Args>
+		struct deduce_lambda<Result(Class::*)(Args...) noexcept> final {
+			using type = Result(Args...) noexcept;
+		};
+
+		template<typename Result, typename Class, typename... Args>
+		struct deduce_lambda<Result(Class::*)(Args...) const noexcept> final {
+			using type = Result(Args...) noexcept;
+		};
+	}
 
 	template<typename Lambda>
 	function_ref(Lambda) -> function_ref<internal::deduce_lambda_t<decltype(&Lambda::operator())>>;

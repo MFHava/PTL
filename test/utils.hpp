790ced1fbc9a1032eaad2f822f903e5d2a3dc28a
MFHava <mfh@live.at>
2021-03-12 01:22:00 +0100

    added input_iterator-adapter for unit tests

--- test/utils.hpp
+++ test/utils.hpp
@@ -5,6 +5,7 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 #pragma once
+#include <iterator>
 #include <stdexcept>
 
 namespace ptl::test {
@@ -21,5 +22,34 @@ namespace ptl::test {
 			other.moved = true;
 			return *this;
 		}
+	};
+
+	template<typename Iterator>
+	class input_iterator final {
+		Iterator it;
+	public:
+		using iterator_category = std::input_iterator_tag;
+		using value_type        = typename Iterator::value_type;
+		using difference_type   = std::ptrdiff_t;
+		using pointer           = typename Iterator::pointer;
+		using reference         = typename Iterator::reference;
+
+		input_iterator() noexcept =default;
+		input_iterator(Iterator it) noexcept : it{it} {}
+
+		auto operator++() noexcept -> input_iterator & { ++it; return *this; }
+		auto operator++(int) noexcept -> input_iterator {
+			auto tmp{*this};
+			++*this;
+			return tmp;
+		}
+
+		auto operator*() const noexcept -> reference { return *it; }
+		auto operator->() const noexcept -> pointer { return &**this; }
+
+		friend
+		auto operator==(const input_iterator & lhs, const input_iterator & rhs) noexcept -> bool { return lhs.it == rhs.it; }
+		friend
+		auto operator!=(const input_iterator & lhs, const input_iterator & rhs) noexcept -> bool { return !(lhs == rhs); } //TODO: [C++20] implicitly generated
 	};
 }

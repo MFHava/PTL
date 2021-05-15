36f5fb22ffc5f3a10035db1a94689c23528b8e75
MFHava <mfh@live.at>
2021-05-15 19:57:52 +0200

    refactored function_ref  * fixed deduction issue in function

--- inc/ptl/function_ref.hpp
+++ inc/ptl/function_ref.hpp
@@ -9,23 +9,50 @@
 #include <type_traits>
 
 namespace ptl {
+	//! @brief non-owning reference to a function (either a plain function or a functor)
+	//! @tparam Signature function signature of the referenced functor (including potential noexcept-qualifier)
+	template<typename Signature>
+	struct function_ref;
+
 	namespace internal_function_ref { 
 		static_assert(sizeof(void *) == sizeof(void(*)()));
 		static_assert(sizeof(void *) == sizeof(void(*)() noexcept));
+
+		template<typename>
+		struct is_function_ref_specialization : std::false_type {};
+
+		template<typename Signature>
+		struct is_function_ref_specialization<function_ref<Signature>> : std::true_type {};
+
+		template<typename T>
+		inline
+		constexpr
+		bool is_function_ref_specialization_v{is_function_ref_specialization<T>::value};
 
 		template<bool Noexcept, typename Result, typename... Args> //TODO: [C++??] if noexcept-ness were deducible, this extra class would not be necessary and the code overall would be simpler
 		class function_ref {
 			void * functor;
 			Result (*dispatch)(void *, Args...) noexcept(Noexcept);
-		public:
+
 			template<typename Functor>
+			static
+			constexpr //TODO: [C++20] consteval
+			auto is_compatible_functor() noexcept -> bool {
+				if constexpr(is_function_ref_specialization_v<std::decay_t<Functor>>) return false;
+				if constexpr(Noexcept) return std::is_nothrow_invocable_r_v<Result, Functor &, Args...>;
+				else return std::is_invocable_r_v<Result, Functor &, Args...>;
+			}
+		public:
+			template<typename Functor, typename = std::enable_if_t<is_compatible_functor<Functor>()>> //TODO: [C++20] replace with concepts/requires-clause
 			constexpr
 			function_ref(Functor && func) noexcept {
-				if constexpr(std::is_pointer_v<Functor>) {
-					//TODO: [C++??] precondition(func);
+				if constexpr(std::is_pointer_v<Functor>) { //TODO: [C++??] precondition(func);
 					functor = reinterpret_cast<void *>(func);
-				} else functor = reinterpret_cast<void *>(std::addressof(func));
-				dispatch = [](void * ctx, Args... args) noexcept(Noexcept) { return (*reinterpret_cast<std::conditional_t<std::is_pointer_v<Functor>, Functor, std::remove_reference_t<Functor> *>>(ctx))(std::forward<Args>(args)...); };
+					dispatch = +[](void * ctx, Args... args) noexcept(Noexcept) { return reinterpret_cast<Functor>(ctx)(std::forward<Args>(args)...); };
+				} else {
+					functor = reinterpret_cast<void *>(std::addressof(func));
+					dispatch = +[](void * ctx, Args... args) noexcept(Noexcept) { return (*reinterpret_cast<std::remove_reference_t<Functor> *>(ctx))(std::forward<Args>(args)...); };
+				}
 			}
 
 			constexpr
@@ -41,50 +68,30 @@ namespace ptl {
 		};
 
 		template<typename Func>
-		struct deduce_lambda;
+		struct deduce_functor;
 
-		template<typename Func>
-		using deduce_lambda_t = typename deduce_lambda<Func>::type;
+		#define PTL_INTERNAL_FUNCTION_REF_DEFINE_DEDUCTION(Const, Noexcept)\
+			template<typename Result, typename Class, typename... Args>\
+			struct deduce_functor<Result(Class::*)(Args...) Const Noexcept> final {\
+				using type = Result(Args...) Noexcept;\
+			}
+		PTL_INTERNAL_FUNCTION_REF_DEFINE_DEDUCTION(     ,         );
+		PTL_INTERNAL_FUNCTION_REF_DEFINE_DEDUCTION(const,         );
+		PTL_INTERNAL_FUNCTION_REF_DEFINE_DEDUCTION(     , noexcept);
+		PTL_INTERNAL_FUNCTION_REF_DEFINE_DEDUCTION(const, noexcept);
+		#undef PTL_INTERNAL_FUNCTION_REF_DEFINE_DEDUCTION
 
-		template<typename Result, typename Class, typename... Args>
-		struct deduce_lambda<Result(Class::*)(Args...)> final {
-			using type = Result(Args...);
-		};
-
-		template<typename Result, typename Class, typename... Args>
-		struct deduce_lambda<Result(Class::*)(Args...) const> final {
-			using type = Result(Args...);
-		};
-
-		template<typename Result, typename Class, typename... Args>
-		struct deduce_lambda<Result(Class::*)(Args...) noexcept> final {
-			using type = Result(Args...) noexcept;
-		};
-
-		template<typename Result, typename Class, typename... Args>
-		struct deduce_lambda<Result(Class::*)(Args...) const noexcept> final {
-			using type = Result(Args...) noexcept;
-		};
+		//TODO: static_assert(sizeof(function_ref<T>) == 2 * sizeof(void *));
 	}
 
-	//! @brief non-owning reference to a functor (either a plain function or a lambda)
-	//! @tparam Signature function signature of the referenced functor (including potential noexcept-qualifier)
-	template<typename Signature>
-	class function_ref;
-
-	template<typename Result, typename... Args>
-	struct function_ref<Result(Args...)> final : internal_function_ref::function_ref<false, Result, Args...> { //TODO: static_assert(sizeof(function_ref<T>) == 2 * sizeof(void *));
-		template<typename Functor, typename = std::enable_if_t<std::is_invocable_r_v<Result, Functor &, Args...>>>
-		constexpr
-		function_ref(Functor && func) noexcept : internal_function_ref::function_ref<false, Result, Args...>{std::forward<Functor>(func)} {}
-	};
-
-	template<typename Result, typename... Args>
-	struct function_ref<Result(Args...) noexcept> final : internal_function_ref::function_ref<true, Result, Args...> { //TODO: static_assert(sizeof(function_ref<T>) == 2 * sizeof(void *));
-		template<typename Functor, typename = std::enable_if_t<std::is_nothrow_invocable_r_v<Result, Functor &, Args...>>>
-		constexpr
-		function_ref(Functor && func) noexcept : internal_function_ref::function_ref<true, Result, Args...>{std::forward<Functor>(func)} {}
-	};
+	#define PTL_FUNCTION_REF_DEFINE_SPECIALIZATION(Noexcept, FNoexcept)\
+		template<typename Result, typename... Args>\
+		struct function_ref<Result(Args...) Noexcept> final : internal_function_ref::function_ref<FNoexcept, Result, Args...> {\
+			using internal_function_ref::function_ref<FNoexcept, Result, Args...>::function_ref;\
+		}
+	PTL_FUNCTION_REF_DEFINE_SPECIALIZATION(        , 0);
+	PTL_FUNCTION_REF_DEFINE_SPECIALIZATION(noexcept, 1);
+	#undef PTL_FUNCTION_REF_DEFINE_SPECIALIZATION
 
 	template<typename Result, typename... Args>
 	function_ref(Result(*)(Args...)) -> function_ref<Result(Args...)>;
@@ -93,5 +100,5 @@ namespace ptl {
 	function_ref(Result(*)(Args...) noexcept) -> function_ref<Result(Args...) noexcept>;
 
 	template<typename Lambda>
-	function_ref(Lambda) -> function_ref<internal_function_ref::deduce_lambda_t<decltype(&Lambda::operator())>>;
+	function_ref(Lambda) -> function_ref<typename internal_function_ref::deduce_functor<decltype(&Lambda::operator())>::type>;
 }
--- test/function_ref.cpp
+++ test/function_ref.cpp
@@ -17,9 +17,9 @@ namespace {
 	using free_noexcept = int() noexcept;
 
 	int func1a()                 { return 0; }
-	int func1b() noexcept(false) { return 0; }
-	int func2a() noexcept(true)  { return 0; }
-	int func2b() noexcept        { return 0; }
+	int func1b() noexcept(false) { return 1; }
+	int func2a() noexcept(true)  { return 2; }
+	int func2b() noexcept        { return 3; }
 }
 
 TEST_CASE("function_ref free function ref", "[function_ref]") {
@@ -27,13 +27,13 @@ TEST_CASE("function_ref free function ref", "[function_ref]") {
 	REQUIRE(ref1a() == 0);
 	static_assert(std::is_same_v<decltype(ref1a), ptl::function_ref<free_throwing>>);
 	ptl::function_ref ref1b{func1b};
-	REQUIRE(ref1b() == 0);
+	REQUIRE(ref1b() == 1);
 	static_assert(std::is_same_v<decltype(ref1b), ptl::function_ref<free_throwing>>);
 	ptl::function_ref ref2a{func2a};
-	REQUIRE(ref2a() == 0);
+	REQUIRE(ref2a() == 2);
 	static_assert(std::is_same_v<decltype(ref2a), ptl::function_ref<free_noexcept>>);
 	ptl::function_ref ref2b{func2b};
-	REQUIRE(ref2a() == 0);
+	REQUIRE(ref2b() == 3);
 	static_assert(std::is_same_v<decltype(ref2b), ptl::function_ref<free_noexcept>>);
 }
 
@@ -42,86 +42,97 @@ TEST_CASE("function_ref free function ptr", "[function_ref]") {
 	REQUIRE(ref1a() == 0);
 	static_assert(std::is_same_v<decltype(ref1a), ptl::function_ref<free_throwing>>);
 	ptl::function_ref ref1b{&func1b};
-	REQUIRE(ref1b() == 0);
+	REQUIRE(ref1b() == 1);
 	static_assert(std::is_same_v<decltype(ref1b), ptl::function_ref<free_throwing>>);
 	ptl::function_ref ref2a{&func2a};
-	REQUIRE(ref2a() == 0);
+	REQUIRE(ref2a() == 2);
 	static_assert(std::is_same_v<decltype(ref2a), ptl::function_ref<free_noexcept>>);
 	ptl::function_ref ref2b{&func2b};
-	REQUIRE(ref2a() == 0);
+	REQUIRE(ref2b() == 3);
 	static_assert(std::is_same_v<decltype(ref2b), ptl::function_ref<free_noexcept>>);
 }
 
 TEST_CASE("function_ref stateless lambda", "[function_ref]") {
 	auto func1a = []                           { return 0; };
-	auto func1b = []()         noexcept(false) { return 0; };
-	auto func2a = []()         noexcept(true)  { return 0; };
-	auto func2b = []()         noexcept        { return 0; };
-	auto func3a = []() mutable                 { return 0; };
-	auto func3b = []() mutable noexcept(false) { return 0; };
-	auto func4a = []() mutable noexcept(true)  { return 0; };
-	auto func4b = []() mutable noexcept        { return 0; };
+	auto func1b = []()         noexcept(false) { return 1; };
+	auto func2a = []()         noexcept(true)  { return 2; };
+	auto func2b = []()         noexcept        { return 3; };
+	auto func3a = []() mutable                 { return 4; };
+	auto func3b = []() mutable noexcept(false) { return 5; };
+	auto func4a = []() mutable noexcept(true)  { return 6; };
+	auto func4b = []() mutable noexcept        { return 7; };
 
 	ptl::function_ref ref1a{func1a};
 	REQUIRE(ref1a() == 0);
 	static_assert(std::is_same_v<decltype(ref1a), ptl::function_ref<free_throwing>>);
 	ptl::function_ref ref1b{func1b};
-	REQUIRE(ref1b() == 0);
+	REQUIRE(ref1b() == 1);
 	static_assert(std::is_same_v<decltype(ref1b), ptl::function_ref<free_throwing>>);
 	ptl::function_ref ref2a{func2a};
-	REQUIRE(ref2a() == 0);
+	REQUIRE(ref2a() == 2);
 	static_assert(std::is_same_v<decltype(ref2a), ptl::function_ref<free_noexcept>>);
 	ptl::function_ref ref2b{func2b};
-	REQUIRE(ref2a() == 0);
+	REQUIRE(ref2b() == 3);
 	static_assert(std::is_same_v<decltype(ref2b), ptl::function_ref<free_noexcept>>);
 
 	ptl::function_ref ref3a{func3a};
-	REQUIRE(ref3a() == 0);
+	REQUIRE(ref3a() == 4);
 	static_assert(std::is_same_v<decltype(ref3a), ptl::function_ref<free_throwing>>);
 	ptl::function_ref ref3b{func3b};
-	REQUIRE(ref3b() == 0);
+	REQUIRE(ref3b() == 5);
 	static_assert(std::is_same_v<decltype(ref3b), ptl::function_ref<free_throwing>>);
 	ptl::function_ref ref4a{func4a};
-	REQUIRE(ref4a() == 0);
+	REQUIRE(ref4a() == 6);
 	static_assert(std::is_same_v<decltype(ref4a), ptl::function_ref<free_noexcept>>);
 	ptl::function_ref ref4b{func4b};
-	REQUIRE(ref4a() == 0);
+	REQUIRE(ref4b() == 7);
 	static_assert(std::is_same_v<decltype(ref4b), ptl::function_ref<free_noexcept>>);
 }
 
 TEST_CASE("function_ref stateful lambda", "[function_ref]") {
 	auto func1a = [value{0}]                           { return value; };
-	auto func1b = [value{0}]()         noexcept(false) { return value; };
-	auto func2a = [value{0}]()         noexcept(true)  { return value; };
-	auto func2b = [value{0}]()         noexcept        { return value; };
-	auto func3a = [value{0}]() mutable                 { return value; };
-	auto func3b = [value{0}]() mutable noexcept(false) { return value; };
-	auto func4a = [value{0}]() mutable noexcept(true)  { return value; };
-	auto func4b = [value{0}]() mutable noexcept        { return value; };
+	auto func1b = [value{1}]()         noexcept(false) { return value; };
+	auto func2a = [value{2}]()         noexcept(true)  { return value; };
+	auto func2b = [value{3}]()         noexcept        { return value; };
+	auto func3a = [value{4}]() mutable                 { return value; };
+	auto func3b = [value{5}]() mutable noexcept(false) { return value; };
+	auto func4a = [value{6}]() mutable noexcept(true)  { return value; };
+	auto func4b = [value{7}]() mutable noexcept        { return value; };
 
 	ptl::function_ref ref1a{func1a};
 	REQUIRE(ref1a() == 0);
 	static_assert(std::is_same_v<decltype(ref1a), ptl::function_ref<free_throwing>>);
 	ptl::function_ref ref1b{func1b};
-	REQUIRE(ref1b() == 0);
+	REQUIRE(ref1b() == 1);
 	static_assert(std::is_same_v<decltype(ref1b), ptl::function_ref<free_throwing>>);
 	ptl::function_ref ref2a{func2a};
-	REQUIRE(ref2a() == 0);
+	REQUIRE(ref2a() == 2);
 	static_assert(std::is_same_v<decltype(ref2a), ptl::function_ref<free_noexcept>>);
 	ptl::function_ref ref2b{func2b};
-	REQUIRE(ref2a() == 0);
+	REQUIRE(ref2b() == 3);
 	static_assert(std::is_same_v<decltype(ref2b), ptl::function_ref<free_noexcept>>);
 
 	ptl::function_ref ref3a{func3a};
-	REQUIRE(ref3a() == 0);
+	REQUIRE(ref3a() == 4);
 	static_assert(std::is_same_v<decltype(ref3a), ptl::function_ref<free_throwing>>);
 	ptl::function_ref ref3b{func3b};
-	REQUIRE(ref3b() == 0);
+	REQUIRE(ref3b() == 5);
 	static_assert(std::is_same_v<decltype(ref3b), ptl::function_ref<free_throwing>>);
 	ptl::function_ref ref4a{func4a};
-	REQUIRE(ref4a() == 0);
+	REQUIRE(ref4a() == 6);
 	static_assert(std::is_same_v<decltype(ref4a), ptl::function_ref<free_noexcept>>);
 	ptl::function_ref ref4b{func4b};
-	REQUIRE(ref4a() == 0);
+	REQUIRE(ref4b() == 7);
 	static_assert(std::is_same_v<decltype(ref4b), ptl::function_ref<free_noexcept>>);
+}
+
+TEST_CASE("function_ref swapping", "[function_ref]") {
+	ptl::function_ref func1{func1a};
+	ptl::function_ref func2{func1b};
+	REQUIRE(func1() == 0);
+	REQUIRE(func2() == 1);
+
+	swap(func1, func2);
+	REQUIRE(func1() == 1);
+	REQUIRE(func2() == 0);
 }
